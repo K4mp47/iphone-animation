81736c02216b1f489a004449f646e1fc348e927c
K4mp47 <campagnoloalberto5@gmail.com>
2026-01-18 16:51:27 +0100

    first commit

---
+++ components/Spline.tsx
@@ -0,0 +1,12 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Model() {
+  return (
+    <main className='w-full flex justify-center h-screen'>
+      <Spline
+        scene="https://prod.spline.design/VkJLd2f-r1Gr0j9b/scene.splinecode"
+      />
+    </main>
+  );
+}
+
